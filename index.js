40981854c65c3713ff5051a2b230ba12cc982007
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-04 08:34:06 +0800

    ya

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("ini ada diamana");
+#!/usr/bin/env node
+const { exec } = require('child_process');
+
+/**
+ * 
+ * @param {String} args 
+ * @returns {Promise<{stdout, stderr}>}
+ */
+async function Exec(args) {
+    return new Promise((resolve, reject) => {
+        exec(args, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                stdout,
+                stderr
+            });
+        });
+    });
+}
+
+module.exports = { Exec }
